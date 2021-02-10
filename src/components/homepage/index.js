dcab1ee60bea52b68aabf95497e9800ae2dc301c
whawari <walid.hawari@imaginelabs.me>
2021-02-10 21:38:00 +0200

    [EDIT]: linked buttons to corressponding pages

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,5 +1,6 @@
 // npm packages
 import React from "react";
+import { Link } from "react-router-dom";
 
 // styles
 import "./index.css";
@@ -8,17 +9,27 @@ const Homepage = () => {
   return (
     <section id="homepage">
       <h1>Homepage</h1>
+
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
         saepe quasi perspiciatis? Saepe ab consequuntur harum, voluptatem
         aspernatur magni ea?
       </p>
+
       <div id="homepage-call-to-action-buttons">
-        <button
-          className="call-to-action-button"
-          data-text="View listings"
-        ></button>
-        <button className="call-to-action-button" data-text="Add Listing"></button>
+        <Link to="/listings/view">
+          <button
+            className="call-to-action-button"
+            data-text="View listings"
+          ></button>
+        </Link>
+
+        <Link to="/listings/add">
+          <button
+            className="call-to-action-button"
+            data-text="Add Listing"
+          ></button>
+        </Link>
       </div>
     </section>
   );
