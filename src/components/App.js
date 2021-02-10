2672a6a5a30e5b4f31337f51c9d5b1e43daf060c
whawari <walid.hawari@imaginelabs.me>
2021-02-10 02:07:11 +0200

    [REFACTOR]: refactored code

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import Sidebar from "./sidebar";
 import Header from "./header";
 import Error404 from "./error404";
 import Homepage from "./homepage";
-import AddListing from "./listings/AddListing";
+import AddListing from "./listings/add/AddListing";
 
 const App = () => {
   // const [cookie, setCookie] = useCookies(["language"]);
@@ -25,19 +25,21 @@ const App = () => {
   return (
     <BrowserRouter>
       {isSidebar ? (
-        <Sidebar toggleSidebar={toggleSidebar} isSidebar={isSidebar} />
+        <Sidebar toggleSidebar={ toggleSidebar } />
       ) : (
         ""
       )}
+
       <div
         id="router-components"
         style={{ margin: isSidebar ? "" : "var(--header-height) 0 0" }}
       >
-        <Header toggleSidebar={toggleSidebar} isSidebar={isSidebar} />
+        <Header toggleSidebar={ toggleSidebar } isSidebar={ isSidebar } />
+
         <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/add-listing" component={AddListing} />
-          <Route path="*" component={Error404} />
+          <Route exact path="/" component={ Homepage } />
+          <Route exact path="/listings/add" component={ AddListing } />
+          <Route path="*" component={ Error404 } />
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -11,35 +11,35 @@ const Sidebar = ({ toggleSidebar }) => {
   // ---------------------
   return (
     <nav id="sidebar">
-      <ul>
-        <li id="close-sidebar" onClick={ toggleSidebar }>
+      <ul className="sidebar-unordered-list">
+        <li className="sidebar-list-item" onClick={ toggleSidebar } id="close-sidebar">
           <span>Close</span>
           <i className="fas fa-times"></i>
         </li>
 
-        <li onClick={ toggleSidebar }>
-          <Link to="/">
+        <li className="sidebar-list-item" onClick={ toggleSidebar }>
+          <Link className="sidebar-link" to="/">
             <span>Home</span>
             <i className="fas fa-home"></i>
           </Link>
         </li>
 
-        <li onClick={ toggleSidebar }>
-          <Link to="/">
+        <li className="sidebar-list-item" onClick={ toggleSidebar }>
+          <Link className="sidebar-link" to="/">
             <span>View Listings</span>
             <i className="fas fa-list"></i>
           </Link>
         </li>
 
-        <li onClick={ toggleSidebar }>
-          <Link to="/add-listing">
+        <li className="sidebar-list-item" onClick={ toggleSidebar }>
+          <Link className="sidebar-link" to="/listings/add">
             <span>Add a Listing</span>
             <i className="fas fa-plus"></i>
           </Link>
         </li>
 
-        <li onClick={ toggleSidebar }>
-          <Link to="/">
+        <li className="sidebar-list-item" onClick={ toggleSidebar }>
+          <Link className="sidebar-link" to="/">
             <span>Help</span>
             <i className="fas fa-hands-helping"></i>
           </Link>
