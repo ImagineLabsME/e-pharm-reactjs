f0a34182e416c0574fd1a6e27c06fb550cb06d57
whawari <walid.hawari@imaginelabs.me>
2021-02-10 21:52:25 +0200

    [FEAT]: designed 404 page error

--- src/components/error404/index.js
+++ src/components/error404/index.js
@@ -1,9 +1,29 @@
 // npm packages
 import React from "react";
+import { Link } from "react-router-dom";
 
-// to be designed later
+// styles
+import "./index.css";
+
 const Error404 = () => {
-  return <section>404</section>;
+  return (
+    <section id="page-404">
+      <div className="content-container">
+        <h1>404</h1>
+        <h3>Page Not Found</h3>
+        <p>
+          The page you are looking for might have been removed, had its name
+          changed, or is temporarily unavailable
+        </p>
+        <Link to="/">
+          <button
+            className="call-to-action-button"
+            data-text="HOMEPAGE"
+          ></button>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error404;
