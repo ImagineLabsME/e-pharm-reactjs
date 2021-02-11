4dc9fdebdbab7b9f2fca20a9ab07a82f64afd0e9
whawari <walid.hawari@imaginelabs.me>
2021-02-11 23:59:09 +0200

    [FEAT]: added localization -- server side rendering

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,33 +1,52 @@
 // npm packages
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 // styles
 import "./index.css";
 
 const Homepage = () => {
+  const [cookie] = useCookies(["language"]);
+  const [localization, setLocalization] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/api/content/pages/`,
+        {
+          params: {
+            lang: cookie.language,
+            page_name: "homepage",
+          },
+        }
+      );
+
+      setLocalization(res.data);
+    };
+
+    fetchData();
+  }, [cookie.language]);
+
   return (
     <section id="homepage">
-      <h1>Homepage</h1>
+      <h1>{ localization.title_header }</h1>
 
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
-        saepe quasi perspiciatis? Saepe ab consequuntur harum, voluptatem
-        aspernatur magni ea?
-      </p>
+      <p>{ localization.paragraph }</p>
 
       <div id="homepage-call-to-action-buttons">
         <Link to="/listings/view">
           <button
             className="call-to-action-button"
-            data-text="View listings"
+            data-text={ localization.view_listings_button }
           ></button>
         </Link>
 
         <Link to="/listings/add">
           <button
             className="call-to-action-button"
-            data-text="Add Listing"
+            data-text={ localization.add_listing_button }
           ></button>
         </Link>
       </div>
