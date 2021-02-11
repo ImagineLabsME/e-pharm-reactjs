4c4991f0f24b5173851b203f73ebf7c6aeffb911
whawari <walid.hawari@imaginelabs.me>
2021-02-12 00:08:08 +0200

    [ENHANCEMENT]: localization -- server side rendering

--- src/components/error404/index.js
+++ src/components/error404/index.js
@@ -1,24 +1,47 @@
 // npm packages
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 // styles
 import "./index.css";
 
 const Error404 = () => {
+  const [cookie] = useCookies(["language"]);
+  const [localization, setLocalization] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/api/content/pages/`,
+        {
+          params: {
+            lang: cookie.language,
+            page_name: "404",
+          },
+        }
+      );
+
+      setLocalization(res.data);
+    };
+
+    fetchData();
+  }, [cookie.language]);
+
   return (
     <section id="page-404">
       <div className="content-container">
         <h1>404</h1>
-        <h3>Page Not Found</h3>
-        <p>
-          The page you are looking for might have been removed, had its name
-          changed, or is temporarily unavailable
-        </p>
+
+        <h3>{ localization.subtitle_header }</h3>
+
+        <p>{ localization.paragraph }</p>
+
         <Link to="/">
           <button
             className="call-to-action-button"
-            data-text="HOMEPAGE"
+            data-text={ localization.button }
           ></button>
         </Link>
       </div>
